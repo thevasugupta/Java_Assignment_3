c6a80991c6a6d9e206b382876b95bbf5547f5072
thevasugupta <thevasugupta04@gmail.com>
2025-03-09 20:18:33 +0530

    Final Submission

---
+++ ThreadConcepts/DaemonThreadExample.java
@@ -0,0 +1,34 @@
+package ThreadConcepts;
+
+class DaemonThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i<10;i++){
+            System.out.println("Daemon Thread");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class DaemonThreadExample {
+    public static void main(String[] args) {
+        DaemonThread daemon = new DaemonThread();
+        daemon.setDaemon(true);
+        daemon.start();
+
+        for (int i = 0; i <5; i++){
+            System.out.println("Main Thread");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Main Thread Task Completion");
+    }
+}
---
+++ ThreadConcepts/RunnableThread.java
@@ -0,0 +1,17 @@
+package ThreadConcepts;
+
+
+class HelloWorldRunnable implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello, World!");
+        }
+    }
+}
+public class RunnableThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new HelloWorldRunnable());
+        thread.start();
+    }
+}
